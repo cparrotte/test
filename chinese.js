3686fa8751f3da8538447badca9fbc5395f1622c
cparrotte <94488384+cparrotte@users.noreply.github.com>
2022-07-26 11:03:50 -0400

    Create chinese.js

---
+++ chinese.js
@@ -0,0 +1,27 @@
+
+hy.define(
+    {
+        annotation: {
+            quiz: {
+                button: {
+                    cancel: '跳过测验',
+                    back: '上一题',
+                    next: '下一题',
+                    done: '完成',
+                    submit: '发送',
+                    show_answer: '显示答案',
+                    retry: '重试',
+                },
+                text: {
+                    label: {
+                        answer: '答案',                        
+                        page: '页',
+                        of: 'of',
+                        out_of: 'out of',
+                        correct: '正确',
+                        }
+                }
+            }
+        }
+}
+);
